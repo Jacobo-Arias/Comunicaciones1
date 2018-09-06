63940a4450920a9205d73b4391db27f7f5a3b905
Jacobo-Arias <jacobo.a12@utp.edu.co>
2018-09-06 10:57:08 -0500

    Add files via upload

--- Clase sept 6.py
+++ Clase sept 6.py
@@ -1,11 +1,21 @@
 import sympy as sp
+import math
 
+e = math.e
 T = 6
 W0 = 2*sp.pi/T
 
 t,n = sp.symbols('t n')
 
-a0 = 3*(sp.integrate(t+1,(t,-1,0)) - sp.integrate(t-1,(t,0,1)))/T
+
+ft = (3*(t+1) - 3*(t-1))
+
+fn = sp.integrate(ft*e**(1j*n*W0*t),(t,-1,1))
+
+sp.pprint(fn)
+
+
+'''a0 = 3*(sp.integrate(t+1,(t,-1,0)) - sp.integrate(t-1,(t,0,1)))/T
 
 #an = (sp.integrate((t+1) * sp.cos(n*W0*t),(t,-1,0)) - sp.integrate((t-1) * sp.cos(n*W0*t),(t,0,1))) * (2/T)
 an =(sp.integrate((t+1)*sp.cos(n*W0*t), (t, -1, 0)) - sp.integrate((t-1)*sp.cos(n*W0*t), (t,0,1)))
@@ -19,4 +29,4 @@ for i in range(1,100):
 	gt += (an*sp.cos(n*W0*(t+2))).subs(n,i)
 
 
-sp.plot(gt) 
+sp.plot(gt)'''
