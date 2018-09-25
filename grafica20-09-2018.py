221f24c35cccf85bcd11daa245d47b1e3900fe38
Jacobo-Arias <jacobo.a12@utp.edu.co>
2018-09-24 22:44:48 -0500

    Add files via upload

--- grafica20-09-2018.py
+++ grafica20-09-2018.py
@@ -7,6 +7,6 @@ fn = ((-1j)/n*sp.pi)*(sp.sin((n*wo)/2)/((n*wo)/2))*sp.euler((-1j*n*wo)/2)
 gt = 0
 print(sp.euler)
 for i in range(1, 100):
-    gt += 2*sp.pi*(((-1j)/n*sp.pi)*(sp.sin((n*wo)/2)/((n*wo)/2))*pow(sp.euler, ((-1j*n*wo)/2))).subs(n, i)
-
+    gt += 2*sp.pi*(((-1j)/n*sp.pi)*(sp.sin((n*wo)/2)/((n*wo)/2))*sp.euler((-1j*n*wo)/2)).subs(n, i)
+    gt += 2*sp.pi*(((-1j)/n*sp.pi)*(sp.sin((n*wo)/2)/((n*wo)/2))*sp.euler((-1j*n*wo)/2)).subs(n, i)
 sp.plot(gt)
