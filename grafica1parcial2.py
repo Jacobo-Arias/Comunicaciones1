0117cda7992144a586e0a73f3e2c7902d3b3dda6
alejandro.castrillon <alejandro.castrillon@utp.edu.co>
2018-09-27 11:04:58 -0500

    Corregida primer grafica del segundo parcial

--- grafica1parcial2.py
+++ grafica1parcial2.py
@@ -5,5 +5,5 @@ w = sp.symbols('w')
 to = 1
 gt = 0
 for i in range(0, 30):
-    gt += ((A*T)/2)*((sp.sin((w*T)/4))/((w*T)/4))*sp.cos(w*to).subs(w, i)
+    gt += ((A*T))*(((sp.sin((w*T)/4))/((w*T)/4))**2)*sp.cos(w*to).subs(w, i)
 sp.plot(gt)
